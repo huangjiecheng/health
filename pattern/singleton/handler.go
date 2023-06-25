161d13f5a8f7fcda988ca12bb84ea341036b2d78
huangjiecheng <jiecheng.huang@baishan.com>
2023-06-25 15:51:14 +0800

    Initial commit

--- pattern/singleton/handler.go
+++ pattern/singleton/handler.go
@@ -3,6 +3,7 @@ package singleton
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type Instance struct{}
@@ -12,17 +13,22 @@ var (
 	instance *Instance
 )
 
-func NewInstance() *Instance {
+func NewInstance(i int) *Instance {
 	once.Do(func() {
 		instance = &Instance{}
-		fmt.Println("Inside")
+		fmt.Println(fmt.Sprintf("index: %d 懒加载初始化", i))
+
 	})
-	fmt.Println("Outside")
+	fmt.Println(fmt.Sprintf("index: %d 获取到旧的实例", i))
 	return instance
 }
 
 func Start() {
+	fmt.Println("======单例模式启动======")
 	for i := 0; i < 10; i++ {
-		_ = NewInstance()
+		go func() {
+			_ = NewInstance(i)
+		}()
 	}
+	time.Sleep(10 * time.Second)
 }
